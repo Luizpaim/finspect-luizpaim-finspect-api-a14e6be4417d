c9043a963306b552536c0ceeaf37be8d53863e78
luizpaim <luizpaim@pecege.com>
2022-05-27 14:22:04 -0300

    database

--- src/database.ts
+++ src/database.ts
@@ -40,7 +40,7 @@ export interface IDatabase {
 
 export function init(config: IDataConfiguration): IDatabase {
   (<any>Mongoose).Promise = Promise;
-  Mongoose.connect(config.connectionString);
+  Mongoose.connect("mongodb+srv://finspect-development:development@cluster0.6dkvq.mongodb.net/finspect-dev");
 
   let mongoDb = Mongoose.connection;
 
